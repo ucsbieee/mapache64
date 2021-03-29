310d3ca5157ba5d03376d9c1bb406e4d74fc17e6
E4tHam <43790149+E4tHam@users.noreply.github.com>
2021-03-28 18:44:39 -0700

    es - added Ben Eater's example 6502 code

---
+++ assembly-level/eater.asm
@@ -0,0 +1,25 @@
+; https://youtu.be/oO8_2JJV0B4
+
+        .org $8000      ; ROM takes up address $8000 - $ffff
+
+; === Entrypoint === ;
+entrypoint:
+
+        lda #$ff
+        sta $6002       ; set 6522 PA to output
+
+        lda #%01010101  ; alternating bits
+        sta $6000       ; send to 6522
+
+loop:
+        ror             ; rotate bits
+        sta $6000       ; send to 6522
+
+        jmp loop        ; continue rotating
+
+
+; === Set Entrypoint === ;
+
+        .org $fffc      ;
+        .word entrypoint; 6502 expects entrypoint address at fffc
+        .word $0000     ; padding
