9202d36bb140f55cce2988ddaabf01066dc0b51d
E4tHam <43790149+E4tHam@users.noreply.github.com>
2021-04-03 14:22:46 -0700

    es - updated macro example

--- docs/_code/assembly/macro.asm
+++ docs/_code/assembly/macro.asm
@@ -1,16 +1,5 @@
-
-; ===== Macro Definitions ===== ;
-
-nop3:   .macro      ; macro definition to nop three times
-        nop
-        nop
-        nop
-        .endm       ; end macro definition
-
-
-; ===== Program Start ===== ;
-
-        .org $8000  ; set program start to address $8000
-
-        nop         ; run one nop
-        nop3        ; run three nops
+; copy byte at src to dst
+cp8     .macro src, dst
+        lda src
+        sta dst
+        .endm
