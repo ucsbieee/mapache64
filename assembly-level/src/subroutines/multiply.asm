7f5c42f04ba81c5ab3f83d436bfa317c788c1842
E4tHam <43790149+E4tHam@users.noreply.github.com>
2021-04-03 14:22:05 -0700

    es - added subroutine stubs

---
+++ assembly-level/src/subroutines/multiply.asm
@@ -0,0 +1,26 @@
+
+mul8:
+        pha
+
+        ; TO DO
+
+        pla
+        rts
+
+
+mul16:
+        pha
+
+        ; TO DO
+
+        pla
+        rts
+
+
+mulQ11_4:
+        pha
+
+        ; TO DO
+
+        pla
+        rts
