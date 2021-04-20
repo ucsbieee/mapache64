51bf4efec62dbbb7f9023f80c7aa01a310ddef8b
Ethan Sifferman <ethan@sifferman.dev>
2021-04-20 15:13:38 -0700

    es - added example test cases

---
+++ assembly-level/src/tests/add.asm
@@ -0,0 +1,21 @@
+
+        lda #$1
+        sta INT8_I1
+        sta INT8_I2
+        jsr add8
+
+        lda #$ff
+        sta INT16_I1
+        sta INT16_I2
+        lda #$1
+        sta INT16_I1+1
+        sta INT16_I2+1
+        jsr add16
+
+        lda #$ff
+        sta Q9_6_I1
+        sta Q9_6_I2
+        lda #$1
+        sta Q9_6_I1+1
+        sta Q9_6_I2+1
+        jsr addQ9_6
---
+++ assembly-level/src/tests/max.asm
@@ -0,0 +1,6 @@
+
+; Rom assembles to 32763 bytes (the maximum amount)
+
+        .repeat 32763
+        nop
+        .endr
