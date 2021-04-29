9be2f2e7ec744f28b3af22d707d0426c703a5676
Ethan Sifferman <ethan@sifferman.dev>
2021-04-29 11:13:20 -0700

    es - updated assembly dependency updater

--- arcade-game_template/assembly-level/update.sh
+++ arcade-game_template/assembly-level/update.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
+# Files to download
+Dependencies="
+firmware/interrupts.asm
+firmware/subroutines/add.asm
+firmware/subroutines/divide.asm
+firmware/subroutines/multiply.asm
+firmware/subroutines/subtract.asm
+io.asm
+labels.asm
+macros.asm
+options.asm
+rom.asm
+arcade.asm
+"
 
-# Will be macros.asm, firmware.asm, etc...
-Dependencies="misc/eater.asm"
+ASSEMBLY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Assembly directory: $ASSEMBLY_DIR"
 
-mkdir -v dependencies
+DEPENDENCY_DIR=$ASSEMBLY_DIR"/dependencies"
+echo "Dependency directory: $DEPENDENCY_DIR"
 
 # Download all files from ucsbieee/arcade/main/assembly-level/src
 for file in $Dependencies; do
-    path="dependencies/"`dirname $file`
+    path=$DEPENDENCY_DIR"/"`dirname $file`
     filename=`basename $file`
     mkdir -pv ${path}
     curl -LJO https://raw.githubusercontent.com/ucsbieee/arcade/main/assembly-level/src/$file
     mv $filename $path/$filename
+    echo "Downloaded $filename to $path"
 done
