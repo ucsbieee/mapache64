55ad8a0da4dd9d7fc80ef5bf6cc2a2942a87b7b1
Ethan Sifferman <ethan@sifferman.dev>
2021-04-30 13:00:13 -0700

    es - fixed bug in player,renamed controller to controller1, prepared for merge to main

--- high-level/src/player.js
+++ high-level/src/player.js
@@ -18,7 +18,8 @@ function advanceFrame() {
     timer_then = timer_now - ( timer_delta % timer_interval );
 
     // recieve controller inputs
-    CONTROLLER_Q = CONTROLLER_D;
+    CONTROLLER1_Q = CONTROLLER1_D;
+    CONTROLLER2_Q = CONTROLLER2_D;
 
     /* Run CPU */
     drawingPPU = true;
