2b4350aa96e1303f761849b095be8385d3d2da33
Ethan Sifferman <ethan@sifferman.dev>
2021-05-14 04:17:53 -0700

    es - wait is now called before enabling interrupts

--- assembly-level/src/firmware/interrupts.asm
+++ assembly-level/src/firmware/interrupts.asm
@@ -1,33 +1,31 @@
 
 ; NMI (disabled)
-
 _handle_nmi:
         rti
 
 
 
 ; reset
-
 _handle_reset:
+        sei                     ; disble irq by default
         lda #$ff
         sta FRAME
         sta FRAME+1
         sta FRAME+2
         jsr reset
 _next_frame:    ; handle frame timing
-        sei                     ; disble irq
-        inc_mem FRAME, 3        ; set frame to next value
+        inc_mem FRAME, 3        ; increment frame count
         jsr do_logic            ; do non-vram logic
 
+        wai                     ; jump to irq
         cli                     ; enable irq
-        wai                     ; wait for draw irq
         jsr fill_vram           ; fill vram while in vblank
+        sei                     ; disble irq
         jmp _next_frame         ; repeat
 
 
 
 ; IRQ
-
 _handle_irq:
 _vblank_irq:    ; wait for vblank; clear irq and return
         pha
