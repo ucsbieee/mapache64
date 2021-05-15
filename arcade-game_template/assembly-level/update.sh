fbcbdb42d52537727ba8ef274b578857cd8cf361
Ethan Sifferman <ethan@sifferman.dev>
2021-05-14 17:29:18 -0700

    es - switched shell scripts to use LF

--- arcade-game_template/assembly-level/update.sh
+++ arcade-game_template/assembly-level/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Files to download
 Dependencies="
--- tools/pattern-gen/run.sh
+++ tools/pattern-gen/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # # ball.png at address 0 in PMF
 # ./pattern-gen.py -f examples/ball.png -p JavaScript -a 0 -d PMF
