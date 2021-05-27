6bcabd5a6f1790b7ef27558a9165ee185e503be3
Ethan Sifferman <ethan@sifferman.dev>
2021-05-26 23:52:46 -0700

    es - added fill_BSM

---
+++ hardware-level/src/gpu-max_parallel/modules/pattern-hflipper.v
@@ -0,0 +1,16 @@
+
+/* pattern-hflipper.v */
+
+
+module pattern_hflipper_m (
+    input    [15:0] in,
+    input           hflip,
+    output   [15:0] out
+);
+
+    assign out = ( hflip ) ? {
+            in[ 1: 0], in[ 3: 2], in[ 5: 4], in[ 7: 6],
+            in[ 9: 8], in[11:10], in[13:12], in[15:14]
+        } : in;
+
+endmodule
