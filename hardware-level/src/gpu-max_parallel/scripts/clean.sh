8d36c390c684bfae78fa4640913c2b2193252596
Ethan Sifferman <ethan@sifferman.dev>
2021-07-30 14:47:10 -0700

    [gpu tools] fixed redundancy in `CORE_DIR` path

--- hardware-level/src/gpu-max_parallel/scripts/clean.sh
+++ hardware-level/src/gpu-max_parallel/scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
+CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 
 rm -rf ${CORE_DIR}/fusesoc
