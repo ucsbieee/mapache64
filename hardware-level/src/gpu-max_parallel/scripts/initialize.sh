f5609eaa257f8eb443c694f9d1c4b01313040992
Ethan Sifferman <ethan@sifferman.dev>
2021-07-30 14:45:12 -0700

    [gpu tools] fix initialize.sh bug

--- hardware-level/src/gpu-max_parallel/scripts/initialize.sh
+++ hardware-level/src/gpu-max_parallel/scripts/initialize.sh
@@ -2,9 +2,16 @@
 
 CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 
-[ -d "${CORE_DIR}/fusesoc" ]  && rm -rf ${CORE_DIR}/scripts/clean.sh
+[ -d "${CORE_DIR}/fusesoc" ]  && ${CORE_DIR}/scripts/clean.sh
 mkdir ${CORE_DIR}/fusesoc &> /dev/null
 
 cd ${CORE_DIR}/fusesoc
-fusesoc library add gpu_max_parallel ${CORE_DIR} --sync-type=local
-fusesoc library add e4tham_ffs https://github.com/E4tHam/find_first_set --sync-type=git
+
+{       # try to run fusesoc commands
+    fusesoc library add gpu_max_parallel ${CORE_DIR} --sync-type=local &&
+    fusesoc library add e4tham_ffs https://github.com/E4tHam/find_first_set --sync-type=git
+} || {  # if commands failed, clean
+    >&2 echo "[ERROR in initialize.sh]: Initialization failed."
+    ${CORE_DIR}/scripts/clean.sh
+    exit 1
+}
