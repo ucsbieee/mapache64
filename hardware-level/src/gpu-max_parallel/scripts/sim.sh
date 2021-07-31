86644a6f77bb44189567fe994f6ba487ce35482b
Ethan Sifferman <ethan@sifferman.dev>
2021-07-31 10:45:20 -0700

    [gpu tools] added `.fusesoc_path` and `verify.sh`

--- hardware-level/src/gpu-max_parallel/scripts/sim.sh
+++ hardware-level/src/gpu-max_parallel/scripts/sim.sh
@@ -2,10 +2,7 @@
 
 CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 
-if [ ! -d "${CORE_DIR}/fusesoc" ]
-then
-    ${CORE_DIR}/scripts/initialize.sh
-fi
+${CORE_DIR}/scripts/verify.sh || ${CORE_DIR}/scripts/initialize.sh
 
 cd ${CORE_DIR}/fusesoc
 fusesoc run --target sim ucsbieee:arcade:gpu_max_parallel
