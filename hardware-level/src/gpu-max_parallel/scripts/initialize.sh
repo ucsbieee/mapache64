0719188e0254dd5b4ced58e53efee41d64525f25
Ethan Sifferman <ethan@sifferman.dev>
2021-07-31 10:45:20 -0700

    [gpu tools] added `.fusesoc_path` and `verify.sh`

--- hardware-level/src/gpu-max_parallel/scripts/initialize.sh
+++ hardware-level/src/gpu-max_parallel/scripts/initialize.sh
@@ -2,16 +2,19 @@
 
 CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 
-[ -d "${CORE_DIR}/fusesoc" ]  && ${CORE_DIR}/scripts/clean.sh
-mkdir ${CORE_DIR}/fusesoc &> /dev/null
+${CORE_DIR}/scripts/clean.sh
 
+mkdir ${CORE_DIR}/fusesoc &> /dev/null
 cd ${CORE_DIR}/fusesoc
+
+echo ${CORE_DIR}/fusesoc > ${CORE_DIR}/fusesoc/.fusesoc_path
 
 {       # try to run fusesoc commands
     fusesoc library add gpu_max_parallel ${CORE_DIR} --sync-type=local &&
     fusesoc library add e4tham_ffs https://github.com/E4tHam/find_first_set --sync-type=git
+    exit 0
 } || {  # if commands failed, clean
-    >&2 echo "[ERROR in initialize.sh]: Initialization failed."
+    >&2 echo "[ERROR in initialize.sh]: Initialization failed. Maybe FuseSoC isn't installed?"
     ${CORE_DIR}/scripts/clean.sh
     exit 1
 }
