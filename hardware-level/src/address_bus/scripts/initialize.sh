d1499a938d68cd199f5c1db7dcfa9fe8f0f4d0ac
Ethan Sifferman <ethan@sifferman.dev>
2021-08-03 13:11:29 -0700

    [bus] added `address_bus/`

---
+++ hardware-level/src/address_bus/scripts/initialize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+
+${CORE_DIR}/scripts/clean.sh
+
+mkdir ${CORE_DIR}/fusesoc &> /dev/null
+cd ${CORE_DIR}/fusesoc
+
+echo ${CORE_DIR}/fusesoc > ${CORE_DIR}/fusesoc/.fusesoc_path
+
+{       # try to run fusesoc commands
+    fusesoc library add address_bus ${CORE_DIR} --sync-type=local &&
+    exit 0
+} || {  # if commands failed, clean
+    >&2 echo "[ERROR in initialize.sh]: Initialization failed. Maybe FuseSoC isn't installed?"
+    ${CORE_DIR}/scripts/clean.sh
+    exit 1
+}
