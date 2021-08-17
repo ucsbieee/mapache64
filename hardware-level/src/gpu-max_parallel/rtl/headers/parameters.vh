c2d3bcd2db07fb5182401c1f6c15f5a25e138b19
Ethan Sifferman <ethan@sifferman.dev>
2021-08-17 15:45:37 -0700

    [gpu] removed `gpu-max-parallel`, moved `gpu-reduced` to `gpu`

--- hardware-level/src/gpu-max_parallel/rtl/headers/parameters.vh
+++
@@ -1,5 +0,0 @@
-
-`define VRAM_SIZE       12'h900
-`define VRAM_ADDR_WIDTH 12
-
-`define TEST            1
--- hardware-level/src/gpu-reduced/synth/boards/nexys_a7/global_include.tcl
+++
@@ -1,2 +0,0 @@
-set_property file_type "Verilog Header" [get_files parameters.vh]
-set_property is_global_include true [get_files parameters.vh]
