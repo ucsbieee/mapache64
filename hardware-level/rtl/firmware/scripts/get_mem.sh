686ffe9c8adc2e5fddc2e4005c61033009c1f1c8
Ethan Sifferman <ethan@sifferman.dev>
2021-09-05 15:23:30 -0700

    [firmware] moved vectors to firmware

--- hardware-level/rtl/firmware/scripts/get_mem.sh
+++ hardware-level/rtl/firmware/scripts/get_mem.sh
@@ -8,5 +8,12 @@ cd ${ASSEMBLY_SRC_DIR}
 
 make dump
 
+echo "Assembler complete."
+
 mkdir -p ${CORE_DIR}/fusesoc/rom
 xxd -p -c 1 ${ASSEMBLY_SRC_DIR}/dump/firmware.bin ${CORE_DIR}/fusesoc/rom/firmware.mem
+cp ${ASSEMBLY_SRC_DIR}/dump/firmware.bin ${CORE_DIR}/fusesoc/rom/firmware.bin
+xxd -p -c 1 ${ASSEMBLY_SRC_DIR}/dump/vectors.bin ${CORE_DIR}/fusesoc/rom/vectors.mem
+cp ${ASSEMBLY_SRC_DIR}/dump/vectors.bin ${CORE_DIR}/fusesoc/rom/vectors.bin
+
+echo "Rom files copied to \"${CORE_DIR}/fusesoc/rom\"."
