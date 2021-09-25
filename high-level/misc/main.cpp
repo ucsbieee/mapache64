5c87ea8d98d1265a0db07af13596c74fd200eb68
E4tHam <ethan@sifferman.dev>
2021-09-24 23:05:48 -0700

    all high level division implemented

---
+++ high-level/misc/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+#include "Q9_6.hpp"
+#include "division.hpp"
+
+using namespace std;
+
+int main() {
+
+    cout << div8(100,3) << endl;
+    cout << div16(100,3) << endl;
+    cout << divQ9_6(100,3) << endl;
+
+    return 0;
+}
