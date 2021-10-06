9275d9291a21e589a6789095a5756cc8fee6eeb9
E4tHam <ethan@sifferman.dev>
2021-10-06 12:29:53 -0700

    [rtl scripts] improved `get_mem.sh` clarity

--- hardware-level/rtl/firmware/scripts/get_mem.sh
+++ hardware-level/rtl/firmware/scripts/get_mem.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
-
 ASSEMBLY_SRC_DIR="$( cd  "${CORE_DIR}/../../../assembly-level/src" &> /dev/null && pwd )"
 
 cd ${ASSEMBLY_SRC_DIR}
-
 make dump
-
 echo "Assembler complete."
+
 
 DST_DIR=${CORE_DIR}/rom
 
@@ -17,5 +15,4 @@ xxd -p -c 1 ${ASSEMBLY_SRC_DIR}/dump/firmware.bin ${DST_DIR}/firmware.mem
 cp ${ASSEMBLY_SRC_DIR}/dump/firmware.bin ${DST_DIR}/firmware.bin
 xxd -p -c 1 ${ASSEMBLY_SRC_DIR}/dump/vectors.bin ${DST_DIR}/vectors.mem
 cp ${ASSEMBLY_SRC_DIR}/dump/vectors.bin ${DST_DIR}/vectors.bin
-
-echo "Rom files copied to \"${DST_DIR}\"."
+echo "Rom files copied to \"${DST_DIR}/\"."
