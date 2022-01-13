5e9725137ca6998e4ae7b917ef0c7d9789472a9b
E4tHam <ethan@sifferman.dev>
2022-01-13 10:39:11 -0800

    [firmware] removed SIM warning from controller.h

--- assembly-level/src/headers/controller.h
+++ assembly-level/src/headers/controller.h
@@ -2,19 +2,10 @@
 #define __CONTROLLER_H
 
 #include "int.h"
-#ifndef SIM
-#warning "SIM not defined"
-#define SIM 0
-#endif
 
 // Import controller values
-#if SIM
 extern uint8_t CONTROLLER_1;
 extern uint8_t CONTROLLER_2;
-#else
-extern const uint8_t CONTROLLER_1;
-extern const uint8_t CONTROLLER_2;
-#endif
 
 // Masks for controller values
 #define CONTROLLER_A_MASK       ((uint8_t)0x80)
