6a655788a5e19426edc6cd72602e816fcbfbf4bb
Ethan Sifferman <ethan@sifferman.dev>
2023-04-21 22:50:05 -0700

    reorganized RTL; sim works, synth broken

--- hardware-level/rtl/controller_interface/synth/vivado/project_config.tcl
+++
@@ -1,1 +0,0 @@
-set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
--- hardware-level/rtl/top/scripts/sim.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CORE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
-
-${CORE_DIR}/scripts/verify.sh || ${CORE_DIR}/scripts/initialize.sh
-
-cd ${CORE_DIR}/fusesoc
-fusesoc run --target sim ucsbieee:mapache64:top
