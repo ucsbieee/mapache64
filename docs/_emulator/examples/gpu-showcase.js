fb1bc51fccb1b43a9b15d3b8deffa1112bcb857f
Ethan Sifferman <ethan@sifferman.dev>
2023-08-03 15:13:38 -0700

    removed superfluous code

--- docs/_emulator/examples/gpu-showcase.js
+++ docs/_emulator/examples/gpu-showcase.js
@@ -23,8 +23,6 @@ function randomize() {
         PMB[i] = Math.floor(Math.random() * (1 << 8));
     for ( let i = 0; i < NTBL.length; i++ )
         NTBL[i] = Math.floor(Math.random() * (1 << 8));
-    NTBL_setPalette0(Math.floor(Math.random() * (1 << 3)));
-    NTBL_setPalette1(Math.floor(Math.random() * (1 << 3)));
     for ( let i = 0; i < OBM.length; i++ )
         OBM[i] = Math.floor(Math.random() * (1 << 16)) | Math.floor(Math.random() * (1 << 16)) << 16;
     for ( let i = 0; i < TXBL.length; i++ )
